107964a83746276c0cf7bb7542de439d0032b39c
Oliver Fernandez <oliver.fernandez@alumnos.upm.es>
2024-04-10 16:20:53 +0200

    TFM-11 Test unitarios modelo user

--- test/unit/models/user.model.spec.js
+++ test/unit/models/user.model.spec.js
@@ -1,12 +1,42 @@
+const SequelizeMock = require('sequelize-mock');
 const User = require('../../../src/models/user.model.js');
-const sequelize = require('../../../src/models/index.js');
+
+// Setup the mock database connection
+const DBConnectionMock = new SequelizeMock();
+
+// Mocks the User model
+const UserMock = DBConnectionMock.define('User', {
+    username: 'testuser1',
+    name: 'paco',
+    lastName: 'perez',
+    password: 'password',
+    email: 'testuser1@example.com',
+});
+UserMock.$queryInterface.$useHandler(function(query, queryOptions, done) {
+    if (query === 'findOne') {
+        return UserMock.build({
+            username: 'testuser1',
+            name: 'paco',
+            lastName: 'perez',
+            password: 'password',
+            email: 'testuser1@example.com',
+        });
+    }
+
+    if (query === 'create') {
+        return UserMock.build(queryOptions);
+    }
+});
 
 describe('User Model', () => {
-    it('should create a new user', async () => {
-        /*const users = await User.findAll();
-
-        expect(Array.isArray(users)).toBe(true);
-        expect(users[0]).toHaveProperty('username', 'oli');*/
-        expect(true).toBe(true);
+    test('Find User by username', async () => {
+        const foundUser = await UserMock.findOne({ where: { username: 'testuser1' } });
+        expect(foundUser.username).toBe('testuser1');
+    });
+    
+    test('Create user', async () => {
+        const mockUser = { username: 'testuser2', name: 'john', lastName: 'doe', password: 'password', email: 'testuser2@example.com' };
+        const createdUser = await UserMock.create(mockUser);
+        expect(createdUser.username).toBe('testuser2');
     });
 });
